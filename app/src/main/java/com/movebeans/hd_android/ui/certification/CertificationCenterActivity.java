6a83b55acdb7107c6c6b9f59b19b90c288b46d0a
chenwei <chenwei@njbandou.com>
2017-08-02 11:25:32 +0800

    添加认证中心页面

---
+++ app/src/main/java/com/movebeans/hd_android/ui/certification/CertificationCenterActivity.java
@@ -0,0 +1,23 @@
+package com.movebeans.hd_android.ui.certification;
+
+import com.movebeans.hd_android.R;
+import com.movebeans.hd_android.base.BaseActivity;
+
+/**
+ * ClassName: CertificationCenterActivity
+ * Description: 认证中心页面
+ * Creator: chenwei
+ * Date: 2017/8/2 09:58
+ * Version: 1.0
+ */
+public class CertificationCenterActivity extends BaseActivity {
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_certification_center;
+    }
+}
